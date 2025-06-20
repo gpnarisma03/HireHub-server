f198c5933fd71e49985cbb9158edc4b391ba83e6
Gerald Narisma <geraldpnarisma@gmail.com>
2025-06-20 08:33:28 +0800

    fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-'allowed_origins' => ['https://hirehub-tawny.vercel.app'],
+    'allowed_origins' => [
+        'https://hirehub-tawny.vercel.app',
+    ],
+
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +32,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
